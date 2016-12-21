0fe414415706d286daf7972b8bd338f7162bb63c
lizzigrin <lizgrinspoon@gmail.com>
2016-12-21 17:02:53 -0500

    Create initial db creation script

---
+++ create-db.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+Tests the process to create a PostGreSQL database based on spec from property file.
+"""
+
+# String -> String
+# Return property file with database spec 
+# def get_db_prop(filename): return filename;
+
+def main():
+   # get_db_prop(sys.argv[0])
+   # 
