a52cf0d8c9021ec83454e00d7f3459cff2a07a08
lizzigrin <lizgrinspoon@gmail.com>
2016-12-22 08:43:44 -0500

    get database property name file changes

--- create-db.py
+++ create-db.py
@@ -5,10 +5,23 @@
 Tests the process to create a PostGreSQL database based on spec from property file.
 """
 
-# String -> String
-# Return property file with database spec 
-# def get_db_prop(filename): return filename;
+# List -> String
+# Set the filename return to the name of the database property file with specifications 
+# def get_db_prop([' ']): return 'None';
+def get_prop_file(arglist):
+   if len(arglist) == 1:
+      filename = raw_input("Please provide filename that specifies database properties: ")
+   elif len(arglist) == 2:
+      filename = arglist[1]
+   else:
+      filename = "Please retry this script and supply the database property filename as the first argument"
+   return filename
 
 def main():
-   # get_db_prop(sys.argv[0])
-   # 
+   # set database property filename
+   filename = get_prop_file(sys.argv)
+   print "Your database property filename is: ", filename, "\n"
+   sys.exit()
+   
+if __name__ == "__main__":
+   main()
